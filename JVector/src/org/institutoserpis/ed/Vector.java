13ea0fd3ab6cf3c42ddf016bff3fe684b3af7c21
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-10-18 12:16:21 +0200

    Añadido JVector: propuesto indexOf

---
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -0,0 +1,16 @@
+package org.institutoserpis.ed;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] v = new int[] {16, 12, 15, 14, 17};
+		int index = indexOf(v, 12);
+		System.out.println("index="+index);
+	}
+
+	
+	public static int indexOf(int[] v, int x) {
+		return 3;
+	}
+}
