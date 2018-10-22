94771f3658943708fe25d7b10d2f22e87dbe18fc
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-10-22 09:56:03 +0200

    JVector indexOf usando test unitarios

---
+++ JVector/src/org/institutoserpis/ed/VectorTest.java
@@ -0,0 +1,25 @@
+package org.institutoserpis.ed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VectorTest {
+
+	@Test
+	void indexOf() {
+		int[] v = new int[] {16, 12, 15, 14, 17};
+		assertEquals(0, Vector.indexOf(v, 16));
+		assertEquals(2, Vector.indexOf(v, 15));
+		assertEquals(4, Vector.indexOf(v, 17));
+		assertEquals(-1, Vector.indexOf(v, 33));
+	}
+	
+	@Test
+	void indexOfEmpty() {
+		int[] v = new int[] {};
+		assertEquals(-1, Vector.indexOf(v, 22));
+	}
+	
+
+}
