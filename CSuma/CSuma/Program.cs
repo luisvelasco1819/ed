1ece77eb3eec2ef7049feba57291fe7157ccd6cb
luisvelasco1819 <luis.velasco@institutoserpis.org>
2018-11-19 09:46:44 +0100

    Añadido CSuma con un método readDecimal

---
+++ CSuma/CSuma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSuma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			decimal primerNumero = readDecimal("Primer número: ");
+			decimal segundoNumero = readDecimal("Segundo número: ");
+			decimal suma = primerNumero + segundoNumero;
+            Console.WriteLine("Suma=" + suma);
+        }
+
+		private static decimal readDecimal(string label) {
+			Console.Write(label);
+			string stringDecimal = Console.ReadLine();
+			return decimal.Parse(stringDecimal);
+		}
+    }
+}
