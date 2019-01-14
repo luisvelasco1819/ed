2d2693fe8e68990fce0cf451e1fbe955bc6656bc
luisvelasco1819 <luis.velasco@institutoserpis.org>
2019-01-14 09:44:48 +0100

    Añadido CWindow mostrando el uso de MessageDialog

---
+++ CWindow/CWindow/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+using CWindow;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+		Title = "App maravillosa";
+
+		button.Clicked += delegate {
+			//if (WindowHelper.Confirm(this, "¿Quieres tomar algo"))
+			//Console.WriteLine("Ha dicho Yes");
+			//new MyWindow();
+			vBoxReset.Visible = !vBoxReset.Visible;
+		};
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
